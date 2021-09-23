322e9bb6d970da81476ec4c6138fecad6584bf10
arfan21 <arfan.jadul10@gmail.com>
2021-09-23 21:01:53 +0700

    sesi 10 middleware

--- main.go
+++ main.go
@@ -1,55 +1,105 @@
 package main
 
 import (
-    "encoding/json"
-    "github.com/beevik/etree"
-    "log"
-	// "fmt"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
-​
-type M map[string]interface{}
-​
+
 func main() {
-    doc := etree.NewDocument()
-    if err := doc.ReadFromFile("./data.xml"); err != nil {
-        log.Fatal(err.Error())
-    }
-​
-	root    := doc.SelectElement("website")
-	rows := make([]M, 0)
-​
-	for _, article := range root.FindElements("//article") {
-	    row := make(M)
-	    row["title"] = article.SelectElement("title").Text()
-	    row["url"] = article.SelectElement("url").Text()
-​
-	    categories := make([]string, 0)
-	    for _, category := range article.SelectElements("category") {			
-	        categories = append(categories, category.Text())			
-	    }
-	    row["categories"] = categories
-​
-	    if info := article.SelectAttr("info"); info != nil {
-	        row["info"] = info.Value
-	    }
-​
-	    rows = append(rows, row)
-	}
-​
-	// for _,eachRow := range rows{
-	// 	fmt.Println(eachRow)
-	// }
-	// fmt.Println(rows)
-​
-	bts, err := json.MarshalIndent(rows, "", "  ")
-	if err != nil {
-	    log.Fatal(err)
-	}
-​
-	log.Println(string(bts))
-​
-	popularArticleText := root.FindElement(`//article[@info='popular article']/title`)
-	if popularArticleText != nil {
-	    log.Println("Popular article", popularArticleText.Text())
-	}
+	mux := http.DefaultServeMux
+	mux.HandleFunc("/student", ActionStudent)
+	mux.HandleFunc("/teacher", ActionTeacher)
+
+	var handler http.Handler = mux
+	handler = MiddlewareAuth(mux)
+	handler = MiddlewareAllowOnlyGet(handler)
+
+	fmt.Println("server started at localhost:9001")
+	http.ListenAndServe(":9001", handler)
 }
+
+func ActionStudent(w http.ResponseWriter, r *http.Request) {
+
+	if id := r.URL.Query().Get("id"); id != "" {
+		OutputJSON(w, SelectStudent(id))
+		return
+	}
+
+	OutputJSON(w, GetStudents())
+}
+
+func ActionTeacher(w http.ResponseWriter, r *http.Request) {
+	if id := r.URL.Query().Get("id"); id != "" {
+		OutputJSON(w, SelectStudent(id))
+		return
+	}
+	OutputJSON(w, GetTeachers())
+}
+
+func OutputJSON(w http.ResponseWriter, o interface{}) {
+	res, err := json.Marshal(o)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+// var students = []*Student{}
+
+// type Student struct {
+// 	Id    string
+// 	Name  string
+// 	Grade int32
+// }
+
+// func GetStudents() []*Student {
+// 	return students
+// }
+
+// func SelectStudent(id string) *Student {
+// 	for _, each := range students {
+// 		if each.Id == id {
+// 			return each
+// 		}
+// 	}
+
+// 	return nil
+// }
+
+// func init() {
+// 	students = append(students, &Student{Id: "s001", Name: "bourne", Grade: 2})
+// 	students = append(students, &Student{Id: "s002", Name: "ethan", Grade: 2})
+// 	students = append(students, &Student{Id: "s003", Name: "wick", Grade: 3})
+// }
+
+// const USERNAME = "batman"
+// const PASSWORD = "secret"
+
+// func Auth(w http.ResponseWriter, r *http.Request) bool {
+// 	username, password, ok := r.BasicAuth()
+// 	if !ok {
+// 		w.Write([]byte(`something went wrong`))
+// 		return false
+// 	}
+
+// 	isValid := (username == USERNAME) && (password == PASSWORD)
+// 	if !isValid {
+// 		w.Write([]byte(`wrong username/password`))
+// 		return false
+// 	}
+
+// 	return true
+// }
+
+// func AllowOnlyGET(w http.ResponseWriter, r *http.Request) bool {
+// 	if r.Method != "GET" {
+// 		w.Write([]byte("Only GET is allowed"))
+// 		return false
+// 	}
+
+// 	return true
+// }
--- student.go
+++ student.go
@@ -8,6 +8,27 @@ type Student struct {
 	Grade int32
 }
 
+var teachers = []*Teacher{}
+
+type Teacher struct {
+	Id   string
+	Name string
+}
+
+func GetTeachers() []*Teacher {
+	return teachers
+}
+
+func SelectTeacher(id string) *Teacher {
+	for _, each := range teachers {
+		if each.Id == id {
+			return each
+		}
+	}
+
+	return nil
+}
+
 func GetStudents() []*Student {
 	return students
 }
@@ -26,4 +47,7 @@ func init() {
 	students = append(students, &Student{Id: "s001", Name: "bourne", Grade: 2})
 	students = append(students, &Student{Id: "s002", Name: "ethan", Grade: 2})
 	students = append(students, &Student{Id: "s003", Name: "wick", Grade: 3})
+	teachers = append(teachers, &Teacher{Id: "a1", Name: "Jhon"})
+	teachers = append(teachers, &Teacher{Id: "a2", Name: "Iron"})
+	teachers = append(teachers, &Teacher{Id: "a3", Name: "Thor"})
 }
