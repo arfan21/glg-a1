d7a7ed5ca841f7c3ba99e6816c0bd52219fd7a3c
arfan21 <arfan.jadul10@gmail.com>
2021-09-30 20:37:44 +0700

    sesi 10 add port

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/hacktiv8-golang-jwt
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/arfan21/hacktiv8-golang-jwt/router"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	server := router.StartApp()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 
-	server.Run(":9000")
+	server.Run(fmt.Sprintf(":%s", port))
 }
